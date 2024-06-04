af48fe9230dd50d07812805beeff26739dd09ccb
Sersdc <117858036+Sersdc@users.noreply.github.com>
2024-06-04 22:34:14 +0200

    Update ServiceImpl.java

--- GestionMultas/src/es/ubu/lsi/service/multas/ServiceImpl.java
+++ GestionMultas/src/es/ubu/lsi/service/multas/ServiceImpl.java
@@ -1,6 +1,21 @@
 package es.ubu.lsi.service.multas;
 
 import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import es.ubu.lsi.dao.multas.ConductorDAO;
+import es.ubu.lsi.dao.multas.IncidenciaDAO;
+import es.ubu.lsi.dao.multas.TipoIncidenciaDAO;
+import es.ubu.lsi.model.multas.Conductor;
+import es.ubu.lsi.model.multas.Incidencia;
+import es.ubu.lsi.model.multas.Incidencia.IncidenciaPK;
+import es.ubu.lsi.model.multas.TipoIncidencia;
+import es.ubu.lsi.model.multas.Vehiculo;
+import es.ubu.lsi.service.PersistenceException;
+import es.ubu.lsi.service.PersistenceService;
+
 
 public class ServiceImpl extends PersistenceService implements Service {
 
