130335efaba2bb0a77c5c379262a3f136f7a3e64
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 18:44:54 +0200

    Update Conductor.java

--- GestionMultas/src/es/ubu/lsi/model/multas/Conductor.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/Conductor.java
@@ -25,4 +25,65 @@ public class Conductor {
 	@JoinColumn(name = "idauto")
 	private Vehiculo vehiculo;
 	
+	public Conductor() {
+		
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public DireccionPostal getDireccionPostal() {
+		return direccionPostal;
+	}
+
+	public void setDireccionPostal(DireccionPostal direccionPostal) {
+		this.direccionPostal = direccionPostal;
+	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+
+	@Override
+	public String toString() {
+		return "Conductor [nif=" + nif + ", nombre=" + nombre + ", apellido=" + apellido + ", direccionPostal="
+				+ direccionPostal + ", puntos=" + puntos + ", vehiculo=" + vehiculo + "]";
+	}
+
+	
+	
+	
 }
