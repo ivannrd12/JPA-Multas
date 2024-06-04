1b3425f9912bf16701bd7eefe2345fd8f4e3ae10
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 19:36:23 +0200

    Update ServiceImpl.java

--- GestionMultas/src/es/ubu/lsi/service/multas/ServiceImpl.java
+++ GestionMultas/src/es/ubu/lsi/service/multas/ServiceImpl.java
@@ -1,1 +1,30 @@
+package es.ubu.lsi.service.multas;
 
+import java.util.Date;
+import java.util.List;
+
+import es.ubu.lsi.model.multas.Vehiculo;
+import es.ubu.lsi.service.PersistenceException;
+import es.ubu.lsi.service.PersistenceService;
+
+public class ServiceImpl extends PersistenceService implements Service {
+
+	@Override
+	public void insertarIncidencia(Date fecha, String nif, long tipo) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void indultar(String nif) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Vehiculo> consultarVehiculos() throws PersistenceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
