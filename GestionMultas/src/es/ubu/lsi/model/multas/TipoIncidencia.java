6c93f550b08bd4e444488cd84f8b524940abd186
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 16:08:47 +0200

    Update TipoIncidencia.java

--- GestionMultas/src/es/ubu/lsi/model/multas/TipoIncidencia.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/TipoIncidencia.java
@@ -1,5 +1,17 @@
 package es.ubu.lsi.model.multas;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class TipoIncidencia {
 
+	@Id
+	private Long id;
+	
+	private String descripcion;
+	
+	private int valor;
+	
+	
 }
