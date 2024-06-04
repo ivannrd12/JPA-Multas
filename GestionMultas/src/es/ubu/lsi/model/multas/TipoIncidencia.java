8a4d74081b15c3baf94959d5509ffed0693de141
Sersdc <117858036+Sersdc@users.noreply.github.com>
2024-06-04 19:06:18 +0200

    Update TipoIncidencia.java

--- GestionMultas/src/es/ubu/lsi/model/multas/TipoIncidencia.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/TipoIncidencia.java
@@ -1,9 +1,14 @@
 package es.ubu.lsi.model.multas;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
+@NamedQuery(name="Tipoincidencia.findAll", query="selec t from TIPOINCIDENCIA t")
 public class TipoIncidencia {
 
 	@Id
@@ -13,5 +18,48 @@ public class TipoIncidencia {
 	
 	private int valor;
 	
-	
+	@OneToMany(mappedBy="tipoincidencia")
+	private List<Incidencia> incidencias;
+
+	public TipoIncidencia() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public List<Incidencia> getIncidencias() {
+		return incidencias;
+	}
+
+	public void setIncidencias(List<Incidencia> incidencias) {
+		this.incidencias = incidencias;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoIncidencia [id=" + id + ", descripcion=" + descripcion + ", valor=" + valor + ", incidencias="
+				+ incidencias + "]";
+	}
 }
