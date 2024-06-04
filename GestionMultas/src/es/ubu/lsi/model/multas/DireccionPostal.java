bbe835bb1687f8d78b96d70496241c0d139da125
Sersdc <117858036+Sersdc@users.noreply.github.com>
2024-06-04 16:51:53 +0200

    Update DireccionPostal.java

--- GestionMultas/src/es/ubu/lsi/model/multas/DireccionPostal.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/DireccionPostal.java
@@ -3,12 +3,31 @@ package es.ubu.lsi.model.multas;
 import javax.persistence.Embeddable;
 
 @Embeddable
-public class DireccionPostal {
-
-	private String dirreccion;
-	
-	private String cp;
-	
-	private String ciudad;
-	
+public class DireccionPostal{
+	private String direccion;
+   	private String cp;
+    	private String ciudad;
+    
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	@Override
+	public String toString() {
+		return "DireccionPostal [direccion=" + direccion + ", cp=" + cp + ", ciudad=" + ciudad + "]";
+	}
 }
