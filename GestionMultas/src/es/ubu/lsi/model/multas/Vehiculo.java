17587676eb525e956096eb0bb8ff560025baa5d1
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 15:51:19 +0200

    Update Vehiculo.java

--- GestionMultas/src/es/ubu/lsi/model/multas/Vehiculo.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/Vehiculo.java
@@ -1,1 +1,17 @@
+package es.ubu.lsi.model.multas;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Vehiculo {
+
+	@Id
+	private String idAuto;
+	
+	private String nombre;
+	
+	private DireccionPostal direccionPostal;
+	
+}
 
