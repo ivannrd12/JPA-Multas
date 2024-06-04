00d2267e4a51be6dc76bbf231ae046291f9960a5
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 16:15:50 +0200

    Update DireccionPostal.java

--- GestionMultas/src/es/ubu/lsi/model/multas/DireccionPostal.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/DireccionPostal.java
@@ -1,6 +1,14 @@
 package es.ubu.lsi.model.multas;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class DireccionPostal {
 
+	private String dirreccion;
+	
+	private String cp;
+	
+	private String ciudad;
+	
 }
-
