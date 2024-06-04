5fe820dd6bef9aa910b50e620e1c2307a664b083
Sersdc <117858036+Sersdc@users.noreply.github.com>
2024-06-04 18:34:22 +0200

    Update VehiculoDAO.java

--- GestionMultas/src/es/ubu/lsi/dao/multas/VehiculoDAO.java
+++ GestionMultas/src/es/ubu/lsi/dao/multas/VehiculoDAO.java
@@ -3,22 +3,21 @@ package es.ubu.lsi.dao.multas;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import es.ubu.lsi.dao.JpaDAO;
-import es.ubu.lsi.model.multas.Conductor;
+import es.ubu.lsi.model.multas.Vehiculo;
 
-public class ConductorDAO extends JpaDAO<Conductor, String>{
+public class VehiculoDAO extends JpaDAO<Vehiculo, String>{
 
-	public ConductorDAO(EntityManager em) {
+	public VehiculoDAO(EntityManager em) {
 		super(em);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public List<Conductor> findAll() {
+	public List<Vehiculo> findAll() {
 		// TODO Auto-generated method stub
-		return getEntityManager().createNamedQuery("Conductor.findAll", Conductor.class).getResultList();
-
+		return getEntityManager().createNamedQuery("Vehiculo.findAll", Vehiculo.class).getResultList();
 	}
-
 }
