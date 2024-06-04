1e9c2bcff0aa5a5a542a0aa10ad95ede7bbd1946
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 19:08:48 +0200

    Update Incidencia.java

--- GestionMultas/src/es/ubu/lsi/model/multas/Incidencia.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/Incidencia.java
@@ -1,5 +1,105 @@
-package es.ubu.lsi.model.multas;
+ackage es.ubu.lsi.model.multas;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+@Entity
 public class Incidencia {
 
+	@EmbeddedId
+	private IncidenciaPk id;
+	
+	private String anotacion;
+	
+	@ManyToOne
+	@MapsId("nif")
+	@JoinColumn(name = "nif")
+	private Conductor conductor;
+	
+	@ManyToOne
+	@JoinColumn(name = "idtipo")
+	private TipoIncidencia tipoIncidencia;
+
+	public Incidencia() {
+		
+	}
+	
+	public IncidenciaPk getId() {
+		return id;
+	}
+
+
+	public void setId(IncidenciaPk id) {
+		this.id = id;
+	}
+
+
+	public String getAnotacion() {
+		return anotacion;
+	}
+
+
+	public void setAnotacion(String anotacion) {
+		this.anotacion = anotacion;
+	}
+
+
+	public Conductor getConductor() {
+		return conductor;
+	}
+
+
+	public void setConductor(Conductor conductor) {
+		this.conductor = conductor;
+	}
+
+
+	public TipoIncidencia getTipoIncidencia() {
+		return tipoIncidencia;
+	}
+
+
+	public void setTipoIncidencia(TipoIncidencia tipoIncidencia) {
+		this.tipoIncidencia = tipoIncidencia;
+	}
+
+
+@Embeddable
+public class IncidenciaPk implements Serializable {
+
+	private Date fecha;
+		
+	private String nif;
+
+	public IncidenciaPk() {
+		
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	
+}	
+	
+	
 }
