eee2d463a82e943893739969b2f5b61f6b39be6b
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 16:03:09 +0200

    Update Conductor.java

--- GestionMultas/src/es/ubu/lsi/model/multas/Conductor.java
+++ GestionMultas/src/es/ubu/lsi/model/multas/Conductor.java
@@ -1,5 +1,28 @@
 package es.ubu.lsi.model.multas;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Conductor {
 
+	@Id
+	private String nif;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	@Embedded
+	private DireccionPostal direccionPostal;
+	
+	private int puntos;
+	
+	@ManyToOne
+	@JoinColumn(name = "idauto")
+	private Vehiculo vehiculo;
+	
 }
