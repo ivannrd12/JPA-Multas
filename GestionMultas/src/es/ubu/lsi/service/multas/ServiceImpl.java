0808794dc1d2c15830eed8156407a5570be2447d
Ivan Nebreda <130569667+ivannrd12@users.noreply.github.com>
2024-06-04 20:10:15 +0200

    Update ServiceImpl.java

--- GestionMultas/src/es/ubu/lsi/service/multas/ServiceImpl.java
+++ GestionMultas/src/es/ubu/lsi/service/multas/ServiceImpl.java
@@ -9,10 +9,57 @@ import es.ubu.lsi.service.PersistenceService;
 
 public class ServiceImpl extends PersistenceService implements Service {
 
+	private VehiculoDAO vehiculoDao;
+	
+	private ConductorDAO conductorDAO;
+	
+	private TipoIncidenciaDAO tipoIncidenciaDAO;
+	
+	private IncidenciaDAO incidenciaDAO;
+	
+
 	@Override
 	public void insertarIncidencia(Date fecha, String nif, long tipo) throws PersistenceException {
 		// TODO Auto-generated method stub
+		Conductor conductor = conductorDAO.findById(nif);
 		
+		TipoIncidencia tipoIncidencia = tipoIncidenciaDAO.findById(tipo);
+		
+		if(conductor == null ) {
+			
+			throw new IncidentException(IncidentError.NOT_EXIST_DRIVER);
+			
+		} else if (tipoIncidencia == null) {
+			
+			throw new IncidentException(IncidentError.NOT_EXIST_INCIDENT_TYPE);
+		}
+		
+		int puntosRestantes = conductor.getPuntos() - tipoIncidencia.getValor();
+		
+		if (puntosRestantes < 0) {
+			
+			throw new IncidentException(IncidentError.NOT_AVAILABLE_POINTS);
+		}
+		
+		conductor.setPuntos(puntosRestantes);
+		
+		Incidencia incidencia = new Incidencia();
+		
+		IncidenciaPK id = incidencia.new IncidenciaPK();
+		
+		id.setFecha(fecha);
+		
+		id.setNif(nif);
+		
+		incidencia.setId(id);
+		
+		incidencia.setConductor(conductor);
+		
+		incidencia.setTipoIncidencia(tipoIncidencia);
+		
+		incidencia.setAnotacion("");
+		
+		incidenciaDAO.persist(incidencia);
 	}
 
 	@Override
